ee9a4614dfce57fd2b5d5ee85ff44bf387cc7346
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-06-15 13:01:02 +0500

    'user_repository_and_methods_created'

---
+++ src/main/java/com/jwtspringsecurity/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.jwtspringsecurity.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/jwtspringsecurity/security/user/User.java
@@ -0,0 +1,77 @@
+package com.jwtspringsecurity.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING) // enums are unchangeable special classes
+    // EnumType .String will save its fields values as string not as (0, 1, 2 ...) which is its default behaviour.
+    private Role role; // this will create a column of role in db which will only allow defined roles to enter
+
+    // These methods are defined in UserDetails interface we are overriding these methods in our class as per our
+    // requirements because it is the standard procedure.
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+}
